450b57e560c53d0becdc5a8e4f32670616c89e17
Jacob E. Overgaard <s144029@student.dtu.dk>
2018-05-06 21:02:29 +0200

    Commit before clean-up

--- SchLibComponentParameters/SchLibComponentParameters.js
+++ SchLibComponentParameters/SchLibComponentParameters.js
@@ -1,6 +1,13 @@
 //..............................................................................
-// Summary A simple hello world - an introduction to JScript language.
-// Copyright (c) 2003 by Altium Limited
+// 
+// Author: Jacob E. Overgaard
+// Email: jacob.overgaard.andersen@gmail.com
+//
+// Description: 
+//
+//
+//
+// 
 //..............................................................................
 
 //..............................................................................
@@ -11,8 +18,11 @@ function SetParameter(){
 }
 
 function Main(){
-	var CurrentSheet, Component, Iterator, ParamIterator, Parameter, result
-	var ParamArray
+	var CurrentSheet, Component, Iterator, ParamIterator, Parameter;
+	var ParamArray // Array of the desired parameters
+	var ToAdd // Array of the missing parameters
+
+	var tmp, result, counter, greatloopcounter;
 
 	// Parameters to insert to component
 	ParamArray = [
@@ -28,13 +38,15 @@ function Main(){
 		["Published", ""],
 		["Publisher", ""],
 		["PackageDescription", ""]
-	]
+	];
+	Toadd = [];
+
 	if(SchServer == null){ return; }
 
 	CurrentSheet = SchServer.GetCurrentSchDocument;
 	if(CurrentSheet == null){  return; }
 
-	result = "";
+	result = [];
 
 	Iterator = CurrentSheet.SchLibIterator_Create;
 	Iterator.AddFilter_ObjectSet(MkSet(eSchComponent));
@@ -47,18 +59,69 @@ function Main(){
 
 		Parameter = ParamIterator.FirstSchObject;
 
+		// Cycle through and find the current parameters
 		while(Parameter != null){
 			if(Parameter.Name == "Comment"){
-				result += ""
+				
 			}else{
-				result += Parameter.Name + " " + Parameter.Text + "\n";	
+				result.push(Parameter.Name);
 			}
 			Parameter = ParamIterator.NextSchObject;
 		}
+
+		Parameter = ParamIterator.FirstSchObject;
+		if(Parameter == null){
+			ShowMessage("NULL PARAMETER");
+		}else{
+			ShowMessage("We are not null here");
+		}
+		// Parameter.Name = "Test parameter";
+		
+		// Calculate the missing parameters
+		counter = 0;
+		greatloopcounter = 0;
+		loop1: for(var i = 0; i < ParamArray.length; i++){
+			loop2: for(var j = 0; j < result.length; j++){
+				if(ParamArray[i][0] == result[j]){
+					j = 0;
+					break loop2;
+				}else{
+					counter += 1;
+				}
+				if(j == (result.length - 1)){
+					// ShowMessage("j is result length - 1")
+					// greatloopcounter += 1
+					Parameter.Name = "LatestRevisionDate"
+					// Parameter.Name = ParamArray[i][0];
+					// Parameter = ParamIterator.NextSchObject;
+				}
+			}
+
+		}
+		ShowMessage("length of result " + result.length + "i is:" + i + "counter is " + counter + "greatloopcounter is " + greatloopcounter);
+
+
+		// Parameter = ParamIterator.FirstSchObject;
+
+		// Insert the missing parametrs
+		// while(Parameter != null){
+		// 	if(Parameter.Name == "Comment"){
+		// 	}else{
+		// 		result.push(Parameter.Name);
+		// 	}
+		// 	Parameter = ParamIterator.NextSchObject;
+		// }
+
 	}else{
 		ShowMessage("Oops something went wrong. Seems like Component is NULL")
 	}
-	ShowMessage(result);
+	tmp = "";
+
+	// for (var i = 0; i < result.length; i++){
+	// 	tmp += result[i].toString() + "\n";
+	// }
+
+	// ShowMessage(tmp);
 
 	Component.SchIterator_Destroy(ParamIterator)
 	CurrentSheet.SchIterator_Destroy(Iterator);
